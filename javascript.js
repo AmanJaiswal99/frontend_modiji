36b5762e165fc412f935dce3d97d43b38e639d2f
AmanJaiswal99 <51396136+AmanJaiswal99@users.noreply.github.com>
2020-07-07 13:15:38 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,7 +2,17 @@ function input() {
     var p = prompt('Enter a year mitron');
     var year = parseInt(p);
     switch(year) {
-       case 2014,2015,2016,2017,2018,2019: text ="achhe din for sure";
+       case 2014: text ="achhe din for sure";
+       break;
+       case 2015: text ="achhe din for sure";
+       break;
+       case 2016: text ="achhe din for sure";
+       break;
+       case 2017: text ="achhe din for sure";
+       break;
+       case 2018: text ="achhe din for sure";
+       break;
+       case 2019: text ="achhe din for sure";
        break;
        case 2020: text ="2020 is bad vibes mitro";
        break;
